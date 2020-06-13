ed379717e800a39b6f0d063e95b9415d6533c87a
satackey <21271711+satackey@users.noreply.github.com>
2020-06-14 00:36:01 +0900

    WIP

--- src/JavaScriptActionBuilder/Compilers/TtscTranspiler.ts
+++ src/JavaScriptActionBuilder/Compilers/TtscTranspiler.ts
@@ -1,7 +1,15 @@
 import { Compiler } from './Compiler'
 
 export class TtscTranspiler extends Compiler {
-  makeCompileCommand(): string {
-    
+  private getOutDir(): string {
+    return `ttsc-dist`
+  }
+
+  getCompileCommandFor(aFile: string): [string, string[]] {
+    return [`ttsc --outDir`, [this.getOutDir(), aFile]]
+  }
+
+  getCompiledFilePathFor(aFile: string): string {
+    return `${this.getOutDir()}/${aFile}`
   }
 }
